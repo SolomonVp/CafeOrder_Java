568b3b4780fe05d2980fb1ee392e9f3b43c1d607
SolomonVP <Lum7023@yandex.ru>
2023-06-24 22:22:58 +0300

    End

--- app/src/main/java/com/demo/CreateOrderActivity.java
+++ app/src/main/java/com/demo/CreateOrderActivity.java
@@ -94,7 +94,7 @@ public class CreateOrderActivity extends AppCompatActivity {
         String order = String.format(getString(R.string.order), name, password, drink, optionOfDrink);
         String additions;
         if (builderAdditions.length() > 0) {
-            additions = getString(R.string.need_additions) + builderAdditions.toString();
+            additions = "\n" + getString(R.string.need_additions) + builderAdditions.toString();
         } else {
             additions = "";
         }
